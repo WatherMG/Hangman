94a065c30372f20529785a80134a80396646a40e
WatherMG <dr.wather@gmail.com>
2021-12-14 00:08:24 +0300

    Stage 4/8: Help is on the way

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -3,7 +3,7 @@ import random
 print("H A N G M A N")
 words = 'python', 'java', 'kotlin', 'javascript'
 game_word = random.choice(words)
-if input("Guess the word: ") == game_word:
+if input(f"Guess the word {game_word[:3]}{'-' * (len(game_word) -3 )}: ") == game_word:
     print("You survived!")
 else:
     print("You lost!")
