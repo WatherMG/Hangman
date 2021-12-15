1b1ccab62300abbfced412eb46eb470d1a62fe68
WatherMG <dr.wather@gmail.com>
2021-12-15 19:13:05 +0300

    Stage 5/8: Keep trying

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,9 +1,20 @@
 import random
 
-print("H A N G M A N")
 words = 'python', 'java', 'kotlin', 'javascript'
 game_word = random.choice(words)
-if input(f"Guess the word {game_word[:3]}{'-' * (len(game_word) -3 )}: ") == game_word:
-    print("You survived!")
+attempts = 8
+shadow_word = "-" * len(game_word)
+
+print("H A N G M A N")
+while attempts != 0:
+    print(f"\n{shadow_word}")
+    char = input("Input a letter: ")
+    if char in game_word:
+        dictionary = {pos: value for pos, value in enumerate(game_word) if value == char}
+        for pos, value in dictionary.items():
+            shadow_word = ''.join((shadow_word[:pos], value, shadow_word[pos + 1:]))
+    else:
+        print("That letter doesn't appear in the word")
+    attempts -= 1
 else:
-    print("You lost!")
+    print("\nThanks for playing! \nWe'll see how well you did in the next stage")
