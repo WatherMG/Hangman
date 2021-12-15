e88f5813440ecfc1112b5d15d841a224ea0e4a43
WatherMG <dr.wather@gmail.com>
2021-12-15 20:29:29 +0300

    Stage 6/8: The value of life

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -11,10 +11,18 @@ while attempts != 0:
     char = input("Input a letter: ")
     if char in game_word:
         dictionary = {pos: value for pos, value in enumerate(game_word) if value == char}
-        for pos, value in dictionary.items():
-            shadow_word = ''.join((shadow_word[:pos], value, shadow_word[pos + 1:]))
+        if char not in shadow_word:
+            for pos, value in dictionary.items():
+                shadow_word = ''.join((shadow_word[:pos], value, shadow_word[pos + 1:]))
+        else:
+            print("No improvements")
+            attempts -= 1
     else:
         print("That letter doesn't appear in the word")
-    attempts -= 1
-else:
-    print("\nThanks for playing! \nWe'll see how well you did in the next stage")
+        attempts -= 1
+    if "-" not in shadow_word:
+        print("You guessed the word!\nYou survived!")
+        break
+    if attempts == 0:
+        print("You lost!")
+
